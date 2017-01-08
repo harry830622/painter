3626046ab9eb1ef6ff3e29ccc8f927975a78765a
Harry Chang <harry830622@gmail.com>
2017-01-09 02:56:36 +0800

    Adopt pixi.js

--- index.js
+++ index.js
@@ -1,4 +1,61 @@
-const pixi = require('pixi.js');
+const { dialog } = require('electron').remote;
 
-const renderer = pixi.autoDetectRenderer(3840, 2160, { antialias: true });
-document.querySelector('#pixi').appendChild(renderer.view);
+const fs = require('fs');
+
+const painter = require('./painter.js');
+
+const openFileBtn = document.querySelector('#open-file');
+openFileBtn.addEventListener('click', (e) => {
+  dialog.showOpenDialog({ properties: ['openFile'] }, (filenames) => {
+    const fileName = filenames[0];
+
+    fs.readFile(fileName, 'utf-8', (err, data) => {
+      const painting = JSON.parse(data);
+      painter.draw(painting);
+    });
+  });
+
+  e.preventDefault();
+}, false);
+
+const canvas = painter.canvas;
+const stage = painter.stage;
+
+canvas.addEventListener('wheel', (e) => {
+  const isZoomIn = e.deltaY < 0;
+  const direction = isZoomIn ? 1 : -1;
+  const factor = 1 + direction * 0.1;
+
+  painter.stage.scale.x *= factor;
+  painter.stage.scale.y *= factor;
+});
+
+(function pan() {
+  let isDragging = false;
+  let lastPosition;
+
+  stage.mousedown = (e) => {
+    isDragging = true;
+    lastPosition = { x: e.data.global.x, y: e.data.global.y };
+  };
+
+  stage.mousemove = (e) => {
+    if (isDragging) {
+      const position = e.data.global;
+      const dx = position.x - lastPosition.x;
+      const dy = position.y - lastPosition.y;
+      stage.position.x += dx;
+      stage.position.y += dy;
+
+      lastPosition = { x: position.x, y: position.y };
+    }
+  };
+
+  stage.mouseup = (e) => {
+    isDragging = false;
+  };
+}());
+
+module.exports = {
+  painter,
+};
---
+++ painter.js
@@ -0,0 +1,130 @@
+const pixi = require('pixi.js');
+
+const canvasWidth = 1920;
+const canvasHeight = 1080;
+
+const renderer = pixi.autoDetectRenderer(canvasWidth, canvasHeight, {
+  antialias: true,
+});
+
+renderer.backgroundColor = 0xFFFFFF;
+
+document.querySelector('#pixi').appendChild(renderer.view);
+
+const stage = new pixi.Container();
+stage.interactive = true;
+
+const maxLength = 1000000;
+const transparentBackground = new pixi.Graphics();
+transparentBackground
+  .beginFill(0x000000, 0)
+  .drawRect(0, 0, maxLength, maxLength)
+  .endFill();
+
+const renderTexture =
+  pixi.RenderTexture.create(maxLength, maxLength);
+renderer.render(transparentBackground, renderTexture);
+
+const transparentSprite = new pixi.Sprite(renderTexture);
+transparentSprite.position.x = -1 * maxLength / 2;
+transparentSprite.position.y = -1 * maxLength / 2;
+stage.addChild(transparentSprite);
+
+let minX = Number.MAX_VALUE;
+let minY = Number.MAX_VALUE;
+let maxX = Number.MIN_VALUE;
+let maxY = Number.MIN_VALUE;
+
+function updateBoundingBox(...points) {
+  points.forEach(({ x, y }) => {
+    minX = Math.min(minX, x);
+    minY = Math.min(minY, y);
+    maxX = Math.max(maxX, x);
+    maxY = Math.max(maxY, y);
+  });
+}
+
+function createLine(line) {
+  const xFrom = line.from.x;
+  const yFrom = line.from.y;
+  const xTo = line.to.x;
+  const yTo = line.to.y;
+
+  updateBoundingBox({ x: xFrom, y: yFrom }, { x: xTo, y: yTo });
+
+  const pen = new pixi.Graphics();
+  pen.lineStyle(1);
+  pen.moveTo(xFrom, yFrom);
+  pen.lineTo(xTo, yTo);
+
+  return pen;
+}
+
+function createPolygon(polygon, color = 0x0000FF) {
+  const pen = new pixi.Graphics();
+
+  if (polygon.points) {
+    polygon.points.forEach((point) => {
+      updateBoundingBox(point);
+    });
+
+    const points = polygon.points.map(({ x, y }) => new pixi.Point(x, y));
+    pen
+      .beginFill(color)
+      .drawPolygon(points)
+      .endFill();
+  }
+
+  return pen;
+}
+
+function create(shape) {
+  switch (shape.type) {
+    case 'line': {
+      const line = shape;
+      return createLine(line);
+    }
+    case 'polygon': {
+      const polygon = shape;
+      return createPolygon(polygon);
+    }
+    default: { break; }
+  }
+
+  return undefined;
+}
+
+function draw(graphic) {
+  const painting = new pixi.Graphics();
+
+  graphic.objects.map(create).forEach((pen) => {
+    painting.addChild(pen);
+  });
+
+  stage.addChild(painting);
+
+  const paintingWidth = maxX - minX;
+  const paintingHeight = maxY - minY;
+  const halfPaintingWidth = paintingWidth / 2;
+  const halfPaintingHeight = paintingHeight / 2;
+  const scale = canvasHeight / paintingHeight;
+
+  stage.scale.x = scale * 0.9;
+  stage.scale.y = scale * 0.9;
+  stage.position.x = halfPaintingWidth * scale;
+  stage.position.y = halfPaintingHeight * scale;
+}
+
+function animate() {
+  renderer.render(stage);
+
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+module.exports = {
+  stage,
+  draw,
+  canvas: renderer.view,
+};
